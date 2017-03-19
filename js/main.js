84cc082a17155e57eb8deeab9546ff8b29118221
MSI\Alexander <alecellis1985@gmail.com>
2017-03-19 18:21:06 -0300

    step 6 almost working

--- js/main.js
+++ js/main.js
@@ -1,26 +1,113 @@
-var debtNs = (function () {
+var debtNs = (function (states_) {
   'use strict';
-
+  var postInfo = {};
+  var isValid = true;
   var nsFunctions = {
-    nextStep: nextStep
+    nextStep: nextStep,
+    isValidPostalCode: isValidPostalCode,
+    fillStates: fillStates,
+    postInfo: postInfo,
+    validateInputShowWarning: validateInputShowWarning,
+    validateInputs: validateInputs,
+    isValid: isValid
   };
 
   return nsFunctions;
 
   function nextStep() {
+    if (!debtNs.isValid) {
+      return;
+    }
     var debtDiv = $(this).parents('.debt');
     var classes = debtDiv.attr('class').split(' ');
     var index = classes[1].split('-')[1];
+    postInfo[index] = $(this).text();
     debtDiv.fadeOut(function () {
       $('.sec-' + (parseInt(index) + 1)).delay(1).fadeIn();
     });
-    console.log('clickedIndx:' + index);
-
-
   }
-})();
+
+  function validateInputs(inputs) {
+    var isValid = true;
+    var length = inputs.length;
+    for (var i = 0; i < length; i++) {
+      if (!validateInputShowWarning(inputs[i])) {
+        isValid = false;
+      }
+    }
+    return isValid;
+  }
+
+  function validateInputShowWarning(input) {
+    var isValid = checkStrVal(input);
+    if (isValid) {
+      $(input).next("p").hide();
+    } else {
+      $(input).next("p").show();
+    }
+    return isValid;
+  }
+
+  function checkStrVal(input) {
+    return input.value !== '' && input.value !== undefined;
+  }
+
+  function fillStates() {
+    $('#states ul').append(createLiList(states_));
+  }
+
+  function isValidPostalCode(input) {
+    var ret = /^([0-9]{5})(?:[-\s]*([0-9]{4}))?$/.test(input.value);
+    if (ret) {
+      $(input).next("p").hide();
+    } else {
+      $(input).next("p").show();
+    }
+    return ret;
+  }
+
+  function createLiList(array) {
+    //Fastest way to add elements to dom
+    var list = [];
+    for (var i = 0; i < array.length; i++) {
+      list[list.length] = "<li id='" + array[i].code + "'>" + array[i].name + "</li>";
+    }
+    return list.join('');
+  }
+
+})(states_);
 
 
 $(document).ready(function () {
+  $('#location').on('click', function (e) {
+    var isValid = true;
+
+    isValid = debtNs.isValidPostalCode($('.sec-4 input')[0]);
+
+    if (debtNs.postInfo.state === undefined) {
+      isValid = false;
+      $('#states p').show();
+    }
+
+    debtNs.isValid = isValid;
+  });
+
+  $('#name').on('click', function (e) {
+    debtNs.isValid = debtNs.validateInputs($('.sec-5 input'));
+  });
+
+  $('#phoneNr').on('click', function (e) {
+    debtNs.isValid = debtNs.validateInputs($('.sec-6 input'));
+  });
+
   $('.debt a').on('click', debtNs.nextStep);
+  debtNs.fillStates();
+  $('#states li').on('click', function () {
+    debtNs.postInfo.state = $(this).attr('id');
+    $('#states p').hide();
+    $('#statesUl span').text($(this).text());
+  });
+
+
+
 });
--- step1.php
+++ step1.php
@@ -117,7 +117,7 @@
           </div> 
           <div class="row text-center">
             <div class="col-md-12 col-sm-6 col-sm-12">
-              <a class="button" href="http://erasedebt.co/step4.php">0-$5,000</a>
+              <a class="button" href="javascript:;">0-$5,000</a>
             </div>
             <div class="col-md-12 col-sm-6 col-xs-12">
               <a class="button" href="http://erasedebt.co/step4.php">$5,001 - $10,000</a>
@@ -153,10 +153,82 @@
             <div class="col-lg-12"><p>Location</p></div>               
           </div> 
           <div class="row text-center">
-            Zip Code <br><br>
-
-            State
+            <div class="col-xs-12">
+              <div class="row">
+                <div class="col-xs-12">
+                  <div class="form-group pwarning">
+                    <input placeholder="Zip Code" class="form-control" onblur="debtNs.isValidPostalCode(this)" />
+                    <p>
+                      <i class="fa fa-times"></i>Zip Code is invalid
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-xs-12">
+                  <div class="form-group">
+                    <div id="states" class="btn-group docola-dropdown xs-top-margin" style="margin: 0" uib-dropdown is-open="filterByDropdown2.isopen">
+                      <button class="button dropdown-toggle" type="button" id="statesUl" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        <span>Select State</span>
+                        <i class="fa fa-angle-down"></i>
+                      </button>
+                      <ul class="dropdown-menu" role="menu" aria-labelledby="statesUl" style="max-height: 300px; overflow-y: auto;"></ul>
+                      <p>
+                        <i class="fa fa-times"></i>State is invalid
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>  
+              <br />
+              <a id="location" class="button pull-left" href="javascript:;">Next</a>
+            </div>
           </div> 
+        </div>
+        <div class="debt sec-5">
+          <div class="row text-center control-label">
+            <div class="col-lg-12"><p>Name</p></div>               
+          </div>
+          <div class="row">
+            <div class="col-xs-12">
+              <div class="form-group pwarning">
+                <input type="text" placeholder="First Name" class="form-control" maxlength="100" onblur="debtNs.validateInputShowWarning(this)" />
+                <p>
+                  <i class="fa fa-times"></i>First Name is invalid
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-xs-12">
+              <div class="form-group pwarning">
+                <input type="text" placeholder="Last Name" class="form-control" maxlength="100" onblur="debtNs.validateInputShowWarning(this)" />
+                <p>
+                  <i class="fa fa-times"></i>Last Name is invalid
+                </p>
+              </div>
+            </div>
+          </div>
+          <a id="name" class="button pull-left" href="javascript:;">Next</a>
+        </div>
+
+        <div class="debt sec-6">
+          <div class="row text-center control-label">
+            <div class="col-lg-12"><p>Last Step!</p></div>               
+          </div>
+          <div class="row">
+            <div class="col-xs-12">
+              <div class="form-group pwarning">
+                (<input placeholder="Area Code" type="tel" name="tel1" required size="3" maxlength="3" pattern="\d{3}" title="3-digit area code" />) 
+                <input type="tel" name="tel2" size="3" pattern="\d{3}" required maxlength="3" title="3-digit exchange" /> - 
+                <input type="tel" name="tel3" size="4" pattern="\d{4}" required maxlength="4" title="4-digit subscriber number" />
+                <p>
+                  <i class="fa fa-times"></i>Phone Number is invalid
+                </p>
+              </div>
+            </div>
+          </div>
+          <a id="phoneNr" class="button pull-left" href="javascript:;">Next</a>
         </div>
 
       </div>
@@ -185,6 +257,7 @@
 
     <script src="js/jquery-migrate.min.js"></script>
     <script src="js/loading.js"></script>
+    <script src="js/states.js" type="text/javascript"></script>
     <script src="js/main.js" type="text/javascript"></script>
 
     <!-- Contact Form JavaScript -->
