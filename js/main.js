d2470e05945bf5853c2adefac312673723cfdfdd
MSI\Alexander <alecellis1985@gmail.com>
2017-03-20 18:12:10 -0300

    backend for loans

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
+String.prototype.replaceAll = function (find, replace) {
+  var str = this;
+  return str.replace(new RegExp(find.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'), 'g'), replace);
+};
 var debtNs = (function (states_) {
   'use strict';
   var postInfo = {};
   var isValid = true;
+  var lastStep = 6;
+  var regexPatterns = {
+    '\\d{3}': /\d{3}/,
+    '\\d{4}': /\d{4}/
+  }
+
+  var keyValPostInfoDic = ['channel', 'Recording_URL', 'Debt'];
+
   var nsFunctions = {
     nextStep: nextStep,
     isValidPostalCode: isValidPostalCode,
@@ -18,13 +30,38 @@ var debtNs = (function (states_) {
     if (!debtNs.isValid) {
       return;
     }
+
     var debtDiv = $(this).parents('.debt');
     var classes = debtDiv.attr('class').split(' ');
     var index = classes[1].split('-')[1];
-    postInfo[index] = $(this).text();
-    debtDiv.fadeOut(function () {
-      $('.sec-' + (parseInt(index) + 1)).delay(1).fadeIn();
-    });
+    if (index < 4) {
+      postInfo[keyValPostInfoDic[index - 1]] = $(this).text();
+    }
+    if (parseInt(index) === lastStep) {
+      var formObj = {};
+      $.each($('form').serializeArray(), function (i, input) {
+        formObj[input.name] = input.value;
+      });
+      $.extend(postInfo, formObj);
+      var debtarr = postInfo.Debt.replaceAll('$', '').replaceAll(',', '').split('-');
+      postInfo.Debt = (debtarr[1] - debtarr[0]) / 2;
+      $.ajax({
+        type: "POST",
+        url: 'api/addUser',
+        data: postInfo,
+        success: function (response) {
+          console.log(response);
+          debtDiv.fadeOut(function () {
+            $('.sec-' + (parseInt(index) + 1)).delay(1).fadeIn();
+          });
+        }
+      });
+    } else {
+      debtDiv.fadeOut(function () {
+        $('.sec-' + (parseInt(index) + 1)).delay(1).fadeIn();
+      });
+    }
+    console.log(debtNs.postInfo);
   }
 
   function validateInputs(inputs) {
@@ -41,14 +78,24 @@ var debtNs = (function (states_) {
   function validateInputShowWarning(input) {
     var isValid = checkStrVal(input);
     if (isValid) {
-      $(input).next("p").hide();
+      $(input).parents(".form-group").find('p').hide();
     } else {
-      $(input).next("p").show();
+      $(input).parents(".form-group").find('p').show();
     }
     return isValid;
   }
 
   function checkStrVal(input) {
+    var inp = $(input);
+    if (inp.attr('maxlength') !== undefined && parseInt(inp.val().length > inp.attr('maxlength'))) {
+      return false;
+    }
+    if (inp.attr('minlength') !== undefined && parseInt(inp.attr('minlength')) > inp.val().length) {
+      return false;
+    }
+    if (inp.attr('pattern') !== undefined && !regexPatterns[inp.attr('pattern')].test(input.value)) {
+      return false;
+    }
     return input.value !== '' && input.value !== undefined;
   }
 
@@ -59,9 +106,9 @@ var debtNs = (function (states_) {
   function isValidPostalCode(input) {
     var ret = /^([0-9]{5})(?:[-\s]*([0-9]{4}))?$/.test(input.value);
     if (ret) {
-      $(input).next("p").hide();
+      $(input).parents(".form-group").find('p').hide();
     } else {
-      $(input).next("p").show();
+      $(input).parents(".form-group").find('p').show();
     }
     return ret;
   }
@@ -97,7 +144,15 @@ $(document).ready(function () {
   });
 
   $('#phoneNr').on('click', function (e) {
-    debtNs.isValid = debtNs.validateInputs($('.sec-6 input'));
+    var inputs = $('.sec-6 input');
+    debtNs.isValid = debtNs.validateInputs(inputs);
+    if (debtNs.isValid) {
+      var phones = [];
+      inputs.each(function () {
+        phones.push($(this).val());
+      });
+      debtNs.postInfo.primary_phone = phones.join('');
+    }
   });
 
   $('.debt a').on('click', debtNs.nextStep);
@@ -107,7 +162,5 @@ $(document).ready(function () {
     $('#states p').hide();
     $('#statesUl span').text($(this).text());
   });
-
-
 
 });
--- step1.php
+++ step1.php
@@ -65,172 +65,191 @@
 
     <!-- Services Section -->
     <section id="form" >
-      <div class="container">            
-        <div class="debt sec-1">
-          <div class="row  text-center control-label">
-            <div class="col-lg-12"><p>Type Of Debt</p></div>               
-          </div> 
-          <div class="row text-center">
-            <div class="col-md-12 col-sm-6 col-sm-12">
-              <a class="button" href="javascript:;">Credit Card</a>
-            </div>
-            <div class="col-md-12 col-sm-6 col-xs-12">
-              <a class="button" href="http://erasedebt.co/step2.php">Mortgage</a>
-            </div>
-            <div class="col-md-12 col-sm-6 col-xs-12">
-              <a class="button" href="http://erasedebt.co/step2.php">Auto</a>
-            </div>
-            <div class="col-md-12 col-sm-6 col-xs-12">
-              <a class="button" href="http://erasedebt.co/step2.php">Student Loans</a>
-            </div>
-            <div class="col-md-12 col-sm-6 col-xs-12">
-              <a class="button" href="http://erasedebt.co/step2.php">Other</a>
-            </div>
-          </div>
-        </div>
-        <div class="debt sec-2">
-          <div class="row  text-center control-label">
-            <div class="col-lg-12"><p>Status of your Loans</p></div>               
-          </div> 
-          <div class="row text-center">
-            <div class="col-md-12 col-sm-6 col-sm-12">
-              <a class="button" href="javascript:;">Default</a>
-            </div>
-            <div class="col-md-12 col-sm-6 col-xs-12">
-              <a class="button" href="http://erasedebt.co/step3.php">Current</a>
-            </div>
-            <div class="col-md-12 col-sm-6 col-xs-12">
-              <a class="button" href="http://erasedebt.co/step3.php">Collections</a>
-            </div>
-            <div class="col-md-12 col-sm-6 col-xs-12">
-              <a class="button" href="http://erasedebt.co/step3.php">Behind Payment</a>
-            </div>
-            <div class="col-md-12 col-sm-6 col-xs-12">
-              <a class="button" href="http://erasedebt.co/step3.php">Other</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="debt sec-3">
-          <div class="row  text-center control-label">
-            <div class="col-lg-12"><p>Status of your Loans</p></div>               
-          </div> 
-          <div class="row text-center">
-            <div class="col-md-12 col-sm-6 col-sm-12">
-              <a class="button" href="javascript:;">0-$5,000</a>
-            </div>
-            <div class="col-md-12 col-sm-6 col-xs-12">
-              <a class="button" href="http://erasedebt.co/step4.php">$5,001 - $10,000</a>
-            </div>
-            <div class="col-md-12 col-sm-6 col-xs-12">
-              <a class="button" href="http://erasedebt.co/step4.php">$10,001 - $15,000</a>
-            </div>
-            <div class="col-md-12 col-sm-6 col-xs-12">
-              <a class="button" href="http://erasedebt.co/step4.php">$15,001 - $20,000</a>
-            </div>
-            <div class="col-md-12 col-sm-6 col-xs-12">
-              <a class="button" href="http://erasedebt.co/step4.php">$20,001 - $30,000</a>
-            </div>
-            <div class="col-md-12 col-sm-6 col-xs-12">
-              <a class="button" href="http://erasedebt.co/step4.php">$30,001 - $40,000</a>
-            </div>
-            <div class="col-md-12 col-sm-6 col-xs-12">
-              <a class="button" href="http://erasedebt.co/step4.php">$40,001 - $50,000</a>
-            </div>
-            <div class="col-md-12 col-sm-6 col-xs-12">
-              <a class="button" href="http://erasedebt.co/step4.php">$50,001 - $70,000</a>
-            </div>
-            <div class="col-md-12 col-sm-6 col-xs-12">
-              <a class="button" href="http://erasedebt.co/step4.php">$70,001 - $100,000</a>
-            </div>
-            <div class="col-md-12 col-sm-6 col-xs-12">
-              <a class="button" href="http://erasedebt.co/step4.php">$100,000 + </a>
-            </div>
-          </div>
-        </div>
-        <div class="debt sec-4">
-          <div class="row  text-center control-label">
-            <div class="col-lg-12"><p>Location</p></div>               
-          </div> 
-          <div class="row text-center">
-            <div class="col-xs-12">
-              <div class="row">
-                <div class="col-xs-12">
-                  <div class="form-group pwarning">
-                    <input placeholder="Zip Code" class="form-control" onblur="debtNs.isValidPostalCode(this)" />
-                    <p>
-                      <i class="fa fa-times"></i>Zip Code is invalid
-                    </p>
-                  </div>
-                </div>
+      <div class="container">     
+        <form  name="myform" novalidate="">
+          <div class="debt sec-1">
+            <div class="row  text-center control-label">
+              <div class="col-lg-12"><p>Type Of Debt</p></div>               
+            </div> 
+            <div class="row text-center">
+              <div class="col-md-12 col-sm-6 col-sm-12">
+                <a class="button" href="javascript:;">Credit Card</a>
               </div>
-              <div class="row">
-                <div class="col-xs-12">
-                  <div class="form-group">
-                    <div id="states" class="btn-group docola-dropdown xs-top-margin" style="margin: 0" uib-dropdown is-open="filterByDropdown2.isopen">
-                      <button class="button dropdown-toggle" type="button" id="statesUl" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                        <span>Select State</span>
-                        <i class="fa fa-angle-down"></i>
-                      </button>
-                      <ul class="dropdown-menu" role="menu" aria-labelledby="statesUl" style="max-height: 300px; overflow-y: auto;"></ul>
+              <div class="col-md-12 col-sm-6 col-xs-12">
+                <a class="button" href="http://erasedebt.co/step2.php">Mortgage</a>
+              </div>
+              <div class="col-md-12 col-sm-6 col-xs-12">
+                <a class="button" href="http://erasedebt.co/step2.php">Auto</a>
+              </div>
+              <div class="col-md-12 col-sm-6 col-xs-12">
+                <a class="button" href="http://erasedebt.co/step2.php">Student Loans</a>
+              </div>
+              <div class="col-md-12 col-sm-6 col-xs-12">
+                <a class="button" href="http://erasedebt.co/step2.php">Other</a>
+              </div>
+            </div>
+          </div>
+          <div class="debt sec-2">
+            <div class="row  text-center control-label">
+              <div class="col-lg-12"><p>Status of your Loans</p></div>               
+            </div> 
+            <div class="row text-center">
+              <div class="col-md-12 col-sm-6 col-sm-12">
+                <a class="button" href="javascript:;">Default</a>
+              </div>
+              <div class="col-md-12 col-sm-6 col-xs-12">
+                <a class="button" href="http://erasedebt.co/step3.php">Current</a>
+              </div>
+              <div class="col-md-12 col-sm-6 col-xs-12">
+                <a class="button" href="http://erasedebt.co/step3.php">Collections</a>
+              </div>
+              <div class="col-md-12 col-sm-6 col-xs-12">
+                <a class="button" href="http://erasedebt.co/step3.php">Behind Payment</a>
+              </div>
+              <div class="col-md-12 col-sm-6 col-xs-12">
+                <a class="button" href="http://erasedebt.co/step3.php">Other</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="debt sec-3">
+            <div class="row  text-center control-label">
+              <div class="col-lg-12"><p>Status of your Loans</p></div>               
+            </div> 
+            <div class="row text-center">
+              <div class="col-md-12 col-sm-6 col-sm-12">
+                <a class="button" href="javascript:;">0-$5,000</a>
+              </div>
+              <div class="col-md-12 col-sm-6 col-xs-12">
+                <a class="button" href="http://erasedebt.co/step4.php">$5,001 - $10,000</a>
+              </div>
+              <div class="col-md-12 col-sm-6 col-xs-12">
+                <a class="button" href="http://erasedebt.co/step4.php">$10,001 - $15,000</a>
+              </div>
+              <div class="col-md-12 col-sm-6 col-xs-12">
+                <a class="button" href="http://erasedebt.co/step4.php">$15,001 - $20,000</a>
+              </div>
+              <div class="col-md-12 col-sm-6 col-xs-12">
+                <a class="button" href="http://erasedebt.co/step4.php">$20,001 - $30,000</a>
+              </div>
+              <div class="col-md-12 col-sm-6 col-xs-12">
+                <a class="button" href="http://erasedebt.co/step4.php">$30,001 - $40,000</a>
+              </div>
+              <div class="col-md-12 col-sm-6 col-xs-12">
+                <a class="button" href="http://erasedebt.co/step4.php">$40,001 - $50,000</a>
+              </div>
+              <div class="col-md-12 col-sm-6 col-xs-12">
+                <a class="button" href="http://erasedebt.co/step4.php">$50,001 - $70,000</a>
+              </div>
+              <div class="col-md-12 col-sm-6 col-xs-12">
+                <a class="button" href="http://erasedebt.co/step4.php">$70,001 - $100,000</a>
+              </div>
+              <div class="col-md-12 col-sm-6 col-xs-12">
+                <a class="button" href="http://erasedebt.co/step4.php">$100,000 + </a>
+              </div>
+            </div>
+          </div>
+          <div class="debt sec-4">
+            <div class="row  text-center control-label">
+              <div class="col-lg-12"><p>Location</p></div>               
+            </div> 
+            <div class="row text-center">
+              <div class="col-xs-12">
+                <div class="row">
+                  <div class="col-xs-12 col-sm-6">
+                    <div class="form-group pwarning">
+                      <input placeholder="Zip Code" name="zip" class="form-control" onblur="debtNs.isValidPostalCode(this)" />
                       <p>
-                        <i class="fa fa-times"></i>State is invalid
+                        <i class="fa fa-times"></i>Zip Code is invalid
                       </p>
                     </div>
                   </div>
+                  <div class="col-xs-12 col-sm-6">
+                    <div class="form-group">
+                      <div id="states" class="btn-group docola-dropdown xs-top-margin" style="margin: 0" uib-dropdown is-open="filterByDropdown2.isopen">
+                        <button class="button dropdown-toggle" type="button" id="statesUl" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                          <span>Select State</span>
+                          <i class="fa fa-angle-down"></i>
+                        </button>
+                        <ul class="dropdown-menu" role="menu" aria-labelledby="statesUl" style="max-height: 300px; overflow-y: auto;"></ul>
+                        <p>
+                          <i class="fa fa-times"></i>State is invalid
+                        </p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              </div>  
-              <br />
-              <a id="location" class="button pull-left" href="javascript:;">Next</a>
-            </div>
-          </div> 
-        </div>
-        <div class="debt sec-5">
-          <div class="row text-center control-label">
-            <div class="col-lg-12"><p>Name</p></div>               
+                <br />
+                <a id="location" class="button" href="javascript:;">Next</a>
+              </div>
+            </div> 
           </div>
-          <div class="row">
-            <div class="col-xs-12">
-              <div class="form-group pwarning">
-                <input type="text" placeholder="First Name" class="form-control" maxlength="100" onblur="debtNs.validateInputShowWarning(this)" />
-                <p>
-                  <i class="fa fa-times"></i>First Name is invalid
-                </p>
+          <div class="debt sec-5">
+            <div class="row text-center control-label">
+              <div class="col-lg-12"><p>Name</p></div>               
+            </div>
+            <div class="row">
+              <div class="col-xs-12 col-sm-6">
+                <div class="form-group pwarning">
+                  <input type="text" placeholder="First Name" name="first_name" class="form-control" maxlength="100" onblur="debtNs.validateInputShowWarning(this)" />
+                  <p>
+                    <i class="fa fa-times"></i>First Name is invalid
+                  </p>
+                </div>
+              </div>
+              <div class="col-xs-12 col-sm-6">
+                <div class="form-group pwarning">
+                  <input type="text" placeholder="Last Name" name="last_name" class="form-control" maxlength="100" onblur="debtNs.validateInputShowWarning(this)" />
+                  <p>
+                    <i class="fa fa-times"></i>Last Name is invalid
+                  </p>
+                </div>
               </div>
             </div>
+            <a id="name" class="button" href="javascript:;">Next</a>
           </div>
-          <div class="row">
-            <div class="col-xs-12">
-              <div class="form-group pwarning">
-                <input type="text" placeholder="Last Name" class="form-control" maxlength="100" onblur="debtNs.validateInputShowWarning(this)" />
-                <p>
-                  <i class="fa fa-times"></i>Last Name is invalid
-                </p>
-              </div>
-            </div>
-          </div>
-          <a id="name" class="button pull-left" href="javascript:;">Next</a>
-        </div>
 
-        <div class="debt sec-6">
-          <div class="row text-center control-label">
-            <div class="col-lg-12"><p>Last Step!</p></div>               
-          </div>
-          <div class="row">
-            <div class="col-xs-12">
-              <div class="form-group pwarning">
-                (<input placeholder="Area Code" type="tel" name="tel1" required size="3" maxlength="3" pattern="\d{3}" title="3-digit area code" />) 
-                <input type="tel" name="tel2" size="3" pattern="\d{3}" required maxlength="3" title="3-digit exchange" /> - 
-                <input type="tel" name="tel3" size="4" pattern="\d{4}" required maxlength="4" title="4-digit subscriber number" />
-                <p>
-                  <i class="fa fa-times"></i>Phone Number is invalid
-                </p>
+          <div class="debt sec-6">
+            <div class="row text-center control-label">
+              <div class="col-lg-12"><p>Last Step!</p></div>               
+            </div>
+            <div class="row text-center control-label">
+              <div class="col-xs-12 col-sm-offset-1 col-sm-10">
+                <h3>Phone</h3>
+              </div>               
+            </div>
+            <div class="row">
+              <div class="col-xs-12 form-group phone-number">
+                <div class="col-xs-4 col-sm-3 col-sm-offset-1 dash">
+                  <input placeholder="Area Code" name="area_code" class="form-control" type="tel" size="3" maxlength="3" pattern="\d{3}" title="3-digit area code" onblur="debtNs.validateInputShowWarning(this)" />
+                </div>
+                <div class="col-xs-4 col-sm-3 dash">
+                  <input type="tel" name="exchangeCode" class="form-control" size="3" pattern="\d{3}" required maxlength="3" title="3-digit exchange" onblur="debtNs.validateInputShowWarning(this)" />
+                </div>
+                <div class="col-xs-4 col-sm-4">
+                  <input type="tel" name="subscribernr" class="form-control" size="4" pattern="\d{4}" required maxlength="4" title="4-digit subscriber number" onblur="debtNs.validateInputShowWarning(this)" />
+                </div>
+                <div class="col-xs-4 col-sm-3 col-sm-offset-1">
+                  <p>
+                    <i class="fa fa-times"></i>Phone Number is invalid
+                  </p>
+                </div>
+
               </div>
             </div>
+            <a id="phoneNr" class="button" href="javascript:;">Next</a>
           </div>
-          <a id="phoneNr" class="button pull-left" href="javascript:;">Next</a>
-        </div>
-
+          <div class="debt sec-7">
+            <div class="row  text-center control-label">
+              <div class="col-lg-12"><p>Success</p></div>               
+            </div> 
+            <div class="row text-center">
+              To speak to a certified debt counselor call this special number that gets you<br><br>
+              "To The Front Of The Line"<br><br>
+              800-555-5555
+            </div>
+          </div>
+        </form>
       </div>
     </section>
     <footer>
